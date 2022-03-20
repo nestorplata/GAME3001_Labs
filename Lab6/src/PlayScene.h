f83f5a7aaa247a9218564333312577bbae5bdd77
nestorplata <Nestor.PlataGomez@georgebrown.ca>
2022-03-20 11:58:34 -1000

    cleaned up playscene

--- Lab6/src/PlayScene.h
+++ Lab6/src/PlayScene.h
@@ -3,11 +3,13 @@
 #define __PLAY_SCENE__
 
 #include "Scene.h"
+
 #include "Heuristic.h"
 #include "Target.h"
 #include "SpaceShip.h"
-#include "Tile.h"
-
+//#include "Tile.h"
+#include "Obstacle.h"
+#include "PathNode.h"
 class PlayScene : public Scene
 {
 public:
@@ -24,49 +26,18 @@ private:
 	// IMGUI Function
 	void GUI_Function();
 	std::string m_guiTitle;
-	
 	glm::vec2 m_mousePosition;
+	bool m_isGridEnabled;
 
 	// Game Objects
 	Target* m_pTarget;
 	SpaceShip* m_pSpaceShip;
 
-	//PathFinding Objects and Functions
-	std::vector<Tile*> m_pGrid;
-	bool m_isGridEnabled;
+	//create grid of pathnodes
+	std::vector<PathNode*> m_pGrid;
 
-	//create grid
 	void m_buildGrid();
-	bool m_getGridEnabled() const;
-	void m_setGridEnabled(bool state);
-	void m_computeTileCosts();
 
-	// Calculate the chortest path
-	void m_findShortestPath();
-	void m_displayPathList();
-	void m_resetPathFinding();
-	void m_resetSimulation();
-
-	std::vector<Tile*> m_pOpenList;
-	std::vector<Tile*> m_pClosedList;
-	std::vector<Tile*> m_pPathList;
-	Tile* m_pImpassableTiles[3];
-	
-	// convinience functions to convert world to grid space
-	Tile* m_getTile(int col, int row);
-	Tile* m_getTile(glm::vec2 grid_position);
-
-	//heuristics
-	Heuristic m_currentHeuristic;
-
-	int m_moveCounter = 0;
-	bool m_shipIsMoving = false;
-	bool m_ignoreimpassableTiles = false;
-
-	void m_moveShip();
-
-	static int start_position[2];
-	static int goal_position[2];
 };
 
 #endif /* defined (__PLAY_SCENE__) */
