f51331cd35f558c2308fe9e5c2c84890e822e541
nestorplata <Nestor.PlataGomez@georgebrown.ca>
2022-03-20 11:36:11 -1000

    completed the pathNode class

---
+++ Lab6/src/PathNode.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifndef __PATH_NODE__
+#define __PATH_NODE__
+
+#include "Agent.h"
+
+class PathNode : public Agent
+{
+public:
+	PathNode();
+
+	~PathNode();
+
+	void draw() override;
+	void update() override;
+	void clean() override;
+private:
+
+};
+
+#endif /* defined (__PATH_NODE__)*/
